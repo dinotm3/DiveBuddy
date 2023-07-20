1bea1bef14617069c1addfeb5e7708f9432b0e1f
Dino Tretinjak Mesarić <dtretin@racunarstvo.hr>
2023-07-20 16:47:43 +0200

    cleaned up main activity

--- app/src/main/java/d/tmesaric/divebuddy/MainActivity.kt
+++ app/src/main/java/d/tmesaric/divebuddy/MainActivity.kt
@@ -15,6 +15,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.core.app.ActivityCompat
+import androidx.navigation.NavController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
@@ -51,52 +52,8 @@ class MainActivity : ComponentActivity() {
                 ) {
                     val userLoggedIn = true
                     val navController = rememberNavController()
-                    var startDestination = "sign_in"
-                    val fusedLocationProviderClient: FusedLocationProviderClient
+                    val startDestination = redirect(userLoggedIn)
 
-                    if (userLoggedIn) {
-                        startDestination = "profile"
-                        fusedLocationProviderClient =
-                            LocationServices.getFusedLocationProviderClient(this)
-                        fusedLocationProviderClient.getCurrentLocation(
-                            LocationRequest.QUALITY_HIGH_ACCURACY,
-                            object : CancellationToken() {
-                                override fun onCanceledRequested(p0: OnTokenCanceledListener) =
-                                    CancellationTokenSource().token
-
-                                override fun isCancellationRequested() = false
-                            }).addOnSuccessListener { location: Location? ->
-                            if (location == null)
-                                Toast.makeText(this, "cannot get location", Toast.LENGTH_SHORT)
-                                    .show()
-                            else {
-                                val lat = location.latitude
-                                val long = location.longitude
-
-                                location.latitude = 43.38090
-                                location.longitude = 16.56144
-                                var data = getMockData();
-                                var userName = ""
-                                var distance: Float  = 0F
-                                for (user: User in data) {
-                                    val userLocation: Location = user.lastKnownPosition
-                                    userName = user.name;
-                                    distance = location.distanceTo(userLocation)
-                                }
-                                Toast.makeText(this, "Lat: $lat, Long: $long - Distance to $userName is $distance m", Toast.LENGTH_LONG)
-                                    .show()
-                            }
-                        }
-//                        if (ContextCompat.checkSelfPermission(
-//                                this,
-//                                Manifest.permission.ACCESS_FINE_LOCATION
-//                            ) == PackageManager.PERMISSION_GRANTED
-//                        ) {
-//                            // TODO
-//
-//                        }
-
-                    }
                     NavHost(navController = navController, startDestination = startDestination) {
                         composable("profile") { ProfileScreen(navController) }
                         composable("sign_in") { SignInScreen(navController) }
@@ -107,27 +64,15 @@ class MainActivity : ComponentActivity() {
         }
     }
 
-    fun getMockData(): List<User> {
-        val location1 = Location("location 1")
-        var location2: Location?
-        var location3: Location?
-        var location4: Location?
-        var location5: Location?
+    private fun redirect(userLoggedIn: Boolean): String {
+        var startDestination = "sign_in"
+        if (userLoggedIn) {
+            startDestination = "profile"
 
-
-        location1.latitude = 43.38442
-        location1.longitude = 16.55815
-
-        val user1 =
-            User("1", "User1", "user1@gmail.com", Country.Croatia, location1)
-/*    val user2 = User("2", "User2", "user2@gmail.com", Country.Croatia)
-    val user3 = User("3", "User3", "user3@gmail.com", Country.Croatia)
-    val user4 = User("4", "User4", "user4@gmail.com", Country.Croatia)
-    val user5 = User("5", "User5", "user5@gmail.com", Country.Croatia)*/
-
-        val users = listOf(user1);
-        return users;
+        }
+        return startDestination;
     }
+
 }
 
 
